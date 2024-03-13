c2fef04f43721720e9a017819a00e67c43a06bfa
Ved Gupta <vedgupta@protonmail.com>
2024-03-13 16:20:16 +0530

    Update GROQ API base URL

--- lib/groq.ts
+++ lib/groq.ts
@@ -2,7 +2,8 @@ import OpenAI from "openai";
 
 const grok = new OpenAI({
   apiKey: process.env.GROQ_API_KEY || "",
-  baseURL: "https://api.groq.com/openai/v1",
+  baseURL: "https://api.groq.com/",
+
 });
 
 export default grok;
