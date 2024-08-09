16ad45618658fb06aba02a48130f43115b5d3654
Ved Gupta <vedgupta@protonmail.com>
2024-08-09 08:50:31 +0000

    Refactor page.tsx to import MainPage component and update Home function to include MainPage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import MainPage from "@/components/main";
+
 export default function Home() {
-  return <main className="m-2 overscroll-none"></main>;
+  return (
+    <main className="m-2 overscroll-none">
+      <MainPage />
+    </main>
+  );
 }
