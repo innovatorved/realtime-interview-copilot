94fd98e5dc5caefa1091575a970c7c448e5bdc50
Ved Gupta <vedgupta@protonmail.com>
2024-10-18 13:44:46 +0000

    Refactor Deepgram API integration and update dependencies

---
+++ app/api/actions/deepgram.ts
@@ -0,0 +1,54 @@
+"use server";
+
+const DEEPGRAM_APIKEY = process.env.DEEPGRAM_API_KEY!;
+
+const getTempDeepgramAPIKEY = async () => {
+  const projectsResponse = await fetch("https://api.deepgram.com/v1/projects", {
+    method: "GET",
+    headers: {
+      Authorization: `Token ${DEEPGRAM_APIKEY}`,
+      accept: "application/json",
+    },
+  });
+
+  const projectsResult = await projectsResponse.json();
+
+  if (!projectsResponse.ok) {
+    return projectsResult;
+  }
+
+  const project = projectsResult.projects[0];
+
+  console.log(project);
+  if (!project) {
+    return {
+      error: "Cannot find a Deepgram project. Please create a project first.",
+    };
+  }
+
+  const newKeyResponse = await fetch(
+    `https://api.deepgram.com/v1/projects/${project.project_id}/keys`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Token ${DEEPGRAM_APIKEY}`,
+        accept: "application/json",
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        comment: "Temporary API key",
+        scopes: ["usage:write"],
+        tags: ["next.js"],
+        time_to_live_in_seconds: 10,
+      }),
+    },
+  );
+
+  const newKeyResult = await newKeyResponse.json();
+
+  if (!newKeyResponse.ok) {
+    return newKeyResult;
+  }
+
+  return { ...newKeyResult };
+};
